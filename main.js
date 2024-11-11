aa0f63e71f068c5c903c37d18739a83ab1e31967
Jitendra Kumar <115366789+Jitendra-Kumar116@users.noreply.github.com>
2024-11-11 13:00:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+// Change Navbar style on scroll
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0);
+})
+
+//show hide question answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', ()=> {
+        faq.classList.toggle('open');
+
+        //icons change
+
+        const icon = faq.querySelector('.faq__icon i');
+
+        if(icon.className === 'uil uil-plus'){
+            icon.className ="uil uil-minus"
+        }
+        else{
+            icon.className ="uil uil-plus"
+        }
+    })
+})
+// NavMenu show hide
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',()=>{
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+// close nav menu
+const closeNav = () =>{
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click',closeNav);
